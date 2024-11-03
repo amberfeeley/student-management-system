0a9657c2cea9505e8f5e3e04bbb4f0d7bec7b60f
Amber Feeley <keet@Ambers-Air.attlocal.net>
2024-11-02 21:06:30 -0500

    Added fields, constructors, and methods to Student class

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,53 @@
 public class Student
 {
-    // TODO
+    private int studentID;
+    private String name;
+    private int grade;
+
+    /**
+     * no arg constructor initializes student fields to be empty
+     */
+    public Student()
+    {
+        studentID = 0;
+        name = "";
+        grade = 0;
+    }
+    /**
+     * Constructor with arguments to initialize student
+     * @param id stores studentID
+     * @param n stores student's name
+     * @param g stores student's grade
+     */
+    public Student(int id, String n, int g)
+    {
+        studentID = id;
+        name = n;
+        grade = g;
+    }
+
+    public void setID(int id)
+    {
+        studentID = id;
+    }
+    public void setName(String n)
+    {
+        name = n;
+    }
+    public void setGrade(int g)
+    {
+        grade = g;
+    }
+    public int getID(int id)
+    {
+        return studentID;
+    }
+    public String setName()
+    {
+        return name;
+    }
+    public int setGrade()
+    {
+        return grade;
+    }
 }
